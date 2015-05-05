489e708a8ee448c208569e6aec688b2dd62c5e1e
pdclark <pdclark@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-05 12:13:39 +0000

    Tagging version 1.0.7
    git-svn-id: https://plugins.svn.wordpress.org/styles-twentytwelve/tags/1.0.7@1153714 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/plugin.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Styles: TwentyTwelve
+Plugin URI: http://stylesplugin.com
+Description: Add Customize options to the <a href="http://wordpress.org/extend/themes/twentytwelve" target="_blank">TwentyTwelve theme</a> using the <a href="http://wordpress.org/extend/plugins/styles/" target="_blank">Styles plugin</a>.
+Version: 1.0.7
+Author: Paul Clark
+Author URI: http://pdclark.com
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Require: Styles 1.0.7
+Styles Class: Styles_Child_Theme
+*/
+
+if ( !class_exists( 'Styles_Child_Notices' ) ) {
+    include dirname( __FILE__ ) . '/classes/styles-child-notices/styles-child-notices.php';
+}
